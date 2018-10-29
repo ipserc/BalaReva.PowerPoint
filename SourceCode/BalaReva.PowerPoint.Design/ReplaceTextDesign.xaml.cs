8c70f2aedeca5dfd446c4418a38dca638e429e1c
ipserc <ipserc@users.noreply.github.com>
2018-10-29 10:42:38 +0100

    ReplaceText Activity User Interface for UiPath

---
+++ SourceCode/BalaReva.PowerPoint.Design/ReplaceTextDesign.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Presentation;
+using System.Activities.Presentation.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BalaReva.PowerPoint.Design
+{
+    /// <summary>
+    /// Interaction logic for ReplaceTextDesign.xaml
+    /// </summary>
+    public partial class ReplaceTextDesign : ActivityDesigner
+    {
+        public ReplaceTextDesign()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog _openFileDialog = new OpenFileDialog();
+            _openFileDialog.Title = "Open PowerPiont File";
+            _openFileDialog.Filter = "All PowerPoint Presentations|*.pp*";
+
+            if (_openFileDialog.ShowDialog() ==  DialogResult.OK)
+            {
+                ModelProperty property = this.ModelItem.Properties["FilePath"];
+                //property
+                property.SetValue(new InArgument<string>(_openFileDialog.FileName));
+            }
+        }
+    }
+}
